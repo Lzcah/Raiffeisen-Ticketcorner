47177f047bc730b717d0b873afe8baf905b109ae
franjofranjic <franjo.franjic@edu.gbssg.ch>
2020-05-13 13:21:19 +0200

    Init

---
+++ TicketcornerSrc/src/main/java/ch/raiffeisen/ticketcorner/TicketcornerSrc/TicketcornerSrcApplication.java
@@ -0,0 +1,13 @@
+package ch.raiffeisen.ticketcorner.TicketcornerSrc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketcornerSrcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketcornerSrcApplication.class, args);
+	}
+
+}
---
+++ TicketcornerSrc/src/test/java/ch/raiffeisen/ticketcorner/TicketcornerSrc/TicketcornerSrcApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.raiffeisen.ticketcorner.TicketcornerSrc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketcornerSrcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
